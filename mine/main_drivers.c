720ac49bc9a15e46cfc609a2c9591d522aa6e92f
Charith_Soysa <charith.s@synergenhealth.com>
2021-04-01 04:22:13 +0530

    avx

--- mine/main_drivers.c
+++ mine/main_drivers.c
@@ -75,15 +75,21 @@ void matvec_unrolled_16sse(int n, float *vec_c, const float *mat_a, const float 
 
     for (int i = 0; i < n; i += 1) {
         int j = 0;
-        for (int k = 0; k < unroll16Size; ++k) {
+        for (int k = 0; k < unroll16Size; k++) {
             for (; j < unrolled_num; j += 16) {
 
                 // load next 16 floats from input vector
+                printf("IM here\n");
                 __m512 x = _mm512_load_ps(vec_b + j);
                 // __m512 v = _mm512_load_ps(&vec_b[j]);
+                printf("IM here too\n");
+                printf(:"%d %d %d %d", mat_a, i, n, j);
                 __m512 v = _mm512_load_ps(mat_a + i * n + j);
+                printf("IM here three\n");
                 __m512 xv = _mm512_mul_ps(x, v);
+                printf("IM here 4\n");
                 float result = _mm512_mask_reduce_add_ps(0xFF, xv);
+                printf("IM here 5\n");
                 vec_c[i] = result;
                 printf("%f\n", result);
 
