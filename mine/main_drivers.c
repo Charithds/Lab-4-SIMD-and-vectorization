d845f4e3b5b059b9a7f8e0ba4d48fce9fd1a40d9
Charith_Soysa <charith.s@synergenhealth.com>
2021-04-01 04:23:14 +0530

    in ms

--- mine/main_drivers.c
+++ mine/main_drivers.c
@@ -83,7 +83,7 @@ void matvec_unrolled_16sse(int n, float *vec_c, const float *mat_a, const float 
                 __m512 x = _mm512_load_ps(vec_b + j);
                 // __m512 v = _mm512_load_ps(&vec_b[j]);
                 printf("IM here too\n");
-                printf(:"%d %d %d %d", mat_a, i, n, j);
+                printf("%d %d %d %d", mat_a, i, n, j);
                 __m512 v = _mm512_load_ps(mat_a + i * n + j);
                 printf("IM here three\n");
                 __m512 xv = _mm512_mul_ps(x, v);
