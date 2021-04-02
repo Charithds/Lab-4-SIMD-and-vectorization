626d7239ad0811994225346e297e025cceb7928e
Charith_Soysa <charith.s@synergenhealth.com>
2021-04-02 18:03:33 +0530

    non divisable by 16

--- mine/main_drivers.c
+++ mine/main_drivers.c
@@ -119,44 +119,21 @@ void matvec_unrolled_16sse(int n, float *vec_c, const float *mat_a, const float 
         int j = 0;
 
         for (int k = 0; k < unroll16Size; ++k) {
-            for (; j < unrolled_num; j += 16) {
+            for (; j < unrolled_num; j += 4) {
 
                 // load next 4 floats from input vector
                 __m128 x0 = _mm_load_ps(&vec_b[j]);
-                // load next 4 floats from input vector
-                __m128 x1 = _mm_load_ps(&vec_b[j + 4]);
-                // load next 4 floats from input matrix
                 __m128 v0 = _mm_load_ps(&mat_a[i * n + j]);
-                // load next 4 floats from input matrix
-                __m128 v1 = _mm_load_ps(&mat_a[i * n + j + 4]);
 
                 // Dot product
                 __m128 rslt_m0 = _mm_dp_ps(x0, v0,0xFF);
-                __m128 rslt_m1 = _mm_dp_ps(x1, v1, 0xFF);
-
-                // load next 4 floats from input vector
-                x0 = _mm_load_ps(&vec_b[j + 8]);
-                // load next 4 floats from input vector
-                x1 = _mm_load_ps(&vec_b[j + 12]);
-                // load next 4 floats from input matrix
-                v0 = _mm_load_ps(&mat_a[i * n + j + 8]);
-                // load next 4 floats from input matrix
-                v1 = _mm_load_ps(&mat_a[i * n + j + 12]);
-
-                // Dot product
-                __m128 rslt_m2 = _mm_dp_ps(x0, v0, 0xFF);
-                __m128 rslt_m3 = _mm_dp_ps(x1, v1, 0xFF);
 
                 vec_c[i] += _mm_cvtss_f32(rslt_m0);
-                vec_c[i] += _mm_cvtss_f32(rslt_m1);
-                vec_c[i] += _mm_cvtss_f32(rslt_m2);
-                vec_c[i] += _mm_cvtss_f32(rslt_m3);
-
             }
         }
         if (rest > 0) {
+            int mask = (1 << rest) - 1;
             for (j = unrolled_num; j < n; j += 4) {
-
                 __m128 x0 = _mm_load_ps(&vec_b[j]);
                 __m128 v0 = _mm_load_ps(&mat_a[i * n + j]);
                 __m128 rslt = _mm_dp_ps(x0, v0, 0xFF);
