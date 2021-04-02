fd81616421f00c82ea356b9de03f54ce2d7dab6e
Charith_Soysa <charith.s@synergenhealth.com>
2021-04-02 17:48:57 +0530

    done?

--- mine/main_drivers.h
+++ mine/main_drivers.h
@@ -4,7 +4,7 @@
 // Matrix X Vector versions
 void driveMatVecCPU_listing5(int n);
 
-void driveMatVecCPU_listing6(const float *mat, const float *vec_in, float *vec_out, int n);
+void driveMatVecCPU_listing6(int n);
 
 void driveMatMatCPU_listing7(int n);
 
